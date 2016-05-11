01446483d64a403f0ea784e015b4d5c7e3414504
bevin-pavithran <bevinpavithran@gmail.com>
2016-05-11 14:00:10 +0530

    Performed search using the api class

--- managed_router_api.php
+++ managed_router_api.php
@@ -12,10 +12,11 @@ class ManagedRouterAPI {
 		$this->username = $username;
 		$this->password = $password;
 		$this->display_errors = $display_errors;
-		$this->connect();
+		$this->login();
+		echo "called const";
 	}
 
-	private function connect(){
+	public function login(){
 
 		$data = array("username" => $this->username, "password" => $this->password);
 		$data_string = json_encode($data);                                                                                       
@@ -30,7 +31,36 @@ class ManagedRouterAPI {
 		$result_string = curl_exec($ch);
 		$result = json_decode($result_string);
 		$this->token = $result->token;
+		curl_close($ch);
+		echo "logged in";
 	}
+
+	public function startCurl($url, $params = ''){
+		if($params != '')
+		$params = json_encode($params);
+	
+		$header = array();
+		$header[] = 'Content-Type: application/json';
+		$header[] = 'Content-Length: ' . strlen($params);
+		$header[] = 'Authorization: Bearer '.$this->token;
+
+		$ch = curl_init($url);    
+		var_dump($ch);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");   
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);    
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);                                                 
+		$result = curl_exec($ch);
+		curl_close($ch);
+		return $result;
+	}
+
+	/*public function search($url, $params = ''){
+
+		$result_string = $this->startCurl($url,$params);
+		var_dump($result_string);
+
+	}*/
 
 	
 }
--- test.php
+++ test.php
@@ -1,7 +1,17 @@
 <?php 
 
+if (!ini_get('display_errors')) {
+    ini_set('display_errors', '1');
+}
+
 require_once('managed_router_api.php');
 
 $obj_route = new ManagedRouterAPI("http://192.168.0.7:3000/users/login","bevin","saki322");
+$routers = $obj_route->startCurl("http://192.168.0.7:3000/mr/search",array('RNV5002'));
+/*$routers = $obj_route->startCurl("http://192.168.0.7:3000/mr/search");*/
+echo "<pre>";
+var_dump($routers);
+echo "</pre>"
+
 
 ?>
